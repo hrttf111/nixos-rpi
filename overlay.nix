791df761d76c95effb30a608983d7f6472b6078f
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2023-03-12 14:26:50 +0200

    Fixed 22.11 build

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,27 @@
 { pkgs_x86 }:
 self: super: {
+  ldb = super.ldb.overrideAttrs (oldAttrs: rec {
+    #NOTE(): backported ldb build from later branch:
+    # otherwise the configure script fails with
+    # PYTHONHASHSEED=1 missing! Don't use waf directly, use ./configure and make!
+    preConfigure = ''
+      export PKGCONFIG="$PKG_CONFIG"
+      export PYTHONHASHSEED=1
+    '';
+
+    wafPath = "buildtools/bin/waf";
+
+    wafConfigureFlags = [
+      "--bundled-libraries=NONE"
+      "--builtin-libraries=replace"
+      "--without-ldb-lmdb"
+    ];
+
+    # python-config from build Python gives incorrect values when cross-compiling.
+    # If python-config is not found, the build falls back to using the sysconfig
+    # module, which works correctly in all cases.
+    PYTHON_CONFIG = "/invalid";
+  });
   cups-filters = super.cups-filters.overrideAttrs (oldAttrs: rec {
     configureFlags = oldAttrs.configureFlags ++ [ "--with-cups-config=${super.cups.dev}/bin/cups-config" ];
     nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs_x86.pkg-config super.glib.dev ];
